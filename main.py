329a41de68bfad332e7e91fb6bd40cae39d6d813
nitish-thomas <nitishthomasauz@gmail.com>
2025-12-27 10:28:07 +1100

    added fps

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}") 
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+
+        
+        dt = clock.tick(60) / 1000.0
+        
 
 if __name__ == "__main__":
     main()
